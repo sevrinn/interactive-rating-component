92281de6128e107ded744477bd3af2d003c58bbc
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-27 16:42:57 -0700

    rating number buttons grabbed

--- ratingComponent.js
+++ ratingComponent.js
@@ -1,5 +1,29 @@
-console.log('Hello from the js file')
-
 const submitRating = () => {
 	console.log('submitting your rating...')
 }
+
+const star1 = document.getElementById('1')
+const star2 = document.getElementById('2')
+const star3 = document.getElementById('3')
+const star4 = document.getElementById('4')
+const star5 = document.getElementById('5')
+
+star1.addEventListener('click', (e) => {
+	console.log("i'm star one baby!")
+})
+
+star2.addEventListener('click', (e) => {
+	console.log("i'm star two baby!")
+})
+
+star3.addEventListener('click', (e) => {
+	console.log("i'm star three baby!")
+})
+
+star4.addEventListener('click', (e) => {
+	console.log("i'm star four baby!")
+})
+
+star5.addEventListener('click', (e) => {
+	console.log("i'm star five baby!")
+})
