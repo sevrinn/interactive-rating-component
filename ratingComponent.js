355fce1eaa2e95e2185e7fe0fcd7c82c5d3ce60d
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-27 20:16:52 -0700

    refactored the rating buttons to add class to class list

--- ratingComponent.js
+++ ratingComponent.js
@@ -1,6 +1,4 @@
-const submitRating = () => {
-	console.log('submitting your rating...')
-}
+
 
 const star1 = document.getElementById('1')
 const star2 = document.getElementById('2')
@@ -8,7 +6,7 @@ const star3 = document.getElementById('3')
 const star4 = document.getElementById('4')
 const star5 = document.getElementById('5')
 
-const rating = 0
+let rating = 0
 
 const submitBtn = document.querySelector('.submit-btn')
 
@@ -16,54 +14,55 @@ const orangeBackground = 'background-color: hsl(25, 97%, 53%)'
 
 star1.addEventListener('click', (e) => {
 	const el = e.target
-	el.setAttribute('style', orangeBackground)
-	star2.removeAttribute('style', orangeBackground)
-	star3.removeAttribute('style', orangeBackground)
-	star4.removeAttribute('style', orangeBackground)
-	star5.removeAttribute('style', orangeBackground)
+	el.classList.add('selected')
+	star2.classList.remove('selected')
+	star3.classList.remove('selected')
+	star4.classList.remove('selected')
+	star5.classList.remove('selected')
 	rating = 1
 })
 
 star2.addEventListener('click', (e) => {
 	const el = e.target
-	el.setAttribute('style', orangeBackground)
-	star1.setAttribute('style', orangeBackground)
-	star3.removeAttribute('style', orangeBackground)
-	star4.removeAttribute('style', orangeBackground)
-	star5.removeAttribute('style', orangeBackground)
+	el.classList.add('selected')
+	star1.classList.add('selected')
+	star3.classList.remove('selected')
+	star4.classList.remove('selected')
+	star5.classList.remove('selected')
 	rating = 2
 })
 
 star3.addEventListener('click', (e) => {
 	const el = e.target
-	el.setAttribute('style', orangeBackground)
-	star1.setAttribute('style', orangeBackground)
-	star2.setAttribute('style', orangeBackground)
-	star4.removeAttribute('style', orangeBackground)
-	star5.removeAttribute('style', orangeBackground)
+	el.classList.add('selected')
+	star1.classList.add('selected')
+	star2.classList.add('selected')
+	star4.classList.remove('selected')
+	star5.classList.remove('selected')
 	rating = 3
 })
 
 star4.addEventListener('click', (e) => {
 	const el = e.target
-	el.setAttribute('style', orangeBackground)
-	star1.setAttribute('style', orangeBackground)
-	star2.setAttribute('style', orangeBackground)
-	star3.setAttribute('style', orangeBackground)
-	star5.removeAttribute('style', orangeBackground)
+	el.classList.add('selected')
+	star1.classList.add('selected')
+	star2.classList.add('selected')
+	star3.classList.add('selected')
+	star5.classList.remove('selected')
 	rating = 4
 })
 
 star5.addEventListener('click', (e) => {
 	const el = e.target
-	el.setAttribute('style', orangeBackground)
-	star1.setAttribute('style', orangeBackground)
-	star2.setAttribute('style', orangeBackground)
-	star3.setAttribute('style', orangeBackground)
-	star4.setAttribute('style', orangeBackground)
+	el.classList.add('selected')
+	star1.classList.add('selected')
+	star2.classList.add('selected')
+	star3.classList.add('selected')
+	star4.classList.add('selected')
 	rating = 5
 })
 
 submitBtn.addEventListener('click', (e) => {
 	console.log('submitting your rating...')
+	console.log(rating)
 })
