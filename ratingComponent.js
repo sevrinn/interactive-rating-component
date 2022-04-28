ac84edbe7a9ffdef739d03468d927761fe5dbdca
Sevrinn <sevrinnsevrinn@gmail.com>
2022-04-27 17:54:11 -0700

    rating selection changes background and backgrounds of previous numbers

--- ratingComponent.js
+++ ratingComponent.js
@@ -8,22 +8,62 @@ const star3 = document.getElementById('3')
 const star4 = document.getElementById('4')
 const star5 = document.getElementById('5')
 
+const rating = 0
+
+const submitBtn = document.querySelector('.submit-btn')
+
+const orangeBackground = 'background-color: hsl(25, 97%, 53%)'
+
 star1.addEventListener('click', (e) => {
-	console.log("i'm star one baby!")
+	const el = e.target
+	el.setAttribute('style', orangeBackground)
+	star2.removeAttribute('style', orangeBackground)
+	star3.removeAttribute('style', orangeBackground)
+	star4.removeAttribute('style', orangeBackground)
+	star5.removeAttribute('style', orangeBackground)
+	rating = 1
 })
 
 star2.addEventListener('click', (e) => {
-	console.log("i'm star two baby!")
+	const el = e.target
+	el.setAttribute('style', orangeBackground)
+	star1.setAttribute('style', orangeBackground)
+	star3.removeAttribute('style', orangeBackground)
+	star4.removeAttribute('style', orangeBackground)
+	star5.removeAttribute('style', orangeBackground)
+	rating = 2
 })
 
 star3.addEventListener('click', (e) => {
-	console.log("i'm star three baby!")
+	const el = e.target
+	el.setAttribute('style', orangeBackground)
+	star1.setAttribute('style', orangeBackground)
+	star2.setAttribute('style', orangeBackground)
+	star4.removeAttribute('style', orangeBackground)
+	star5.removeAttribute('style', orangeBackground)
+	rating = 3
 })
 
 star4.addEventListener('click', (e) => {
-	console.log("i'm star four baby!")
+	const el = e.target
+	el.setAttribute('style', orangeBackground)
+	star1.setAttribute('style', orangeBackground)
+	star2.setAttribute('style', orangeBackground)
+	star3.setAttribute('style', orangeBackground)
+	star5.removeAttribute('style', orangeBackground)
+	rating = 4
 })
 
 star5.addEventListener('click', (e) => {
-	console.log("i'm star five baby!")
+	const el = e.target
+	el.setAttribute('style', orangeBackground)
+	star1.setAttribute('style', orangeBackground)
+	star2.setAttribute('style', orangeBackground)
+	star3.setAttribute('style', orangeBackground)
+	star4.setAttribute('style', orangeBackground)
+	rating = 5
+})
+
+submitBtn.addEventListener('click', (e) => {
+	console.log('submitting your rating...')
 })
